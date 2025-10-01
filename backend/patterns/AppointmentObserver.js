1a6eaab3954b03313219efce8694b83174b097f7
Hyojeong Chin <chj9127@gmail.com>
2025-10-01 20:13:24 +1000

    Message Edited

--- backend/patterns/AppointmentObserver.js
+++ backend/patterns/AppointmentObserver.js
@@ -30,7 +30,7 @@ class PetObserver {
         this.petName = petName;
     }
     update(event) {
-        logger.info(`Pet ${this.petName} received notification: ${event}`);
+        logger.info(`Owner ${this.petName} received notification: ${event}`);
     }
 }
 
